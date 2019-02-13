9f71a760bbe8762894b1c65352b11d7d8605adc0
GIRISH <GIRISH@GIRISH-PC>
2019-02-13 14:30:43 +0530

    @girish updated.

---
+++ Dhurga/src/dhurga/D.java
@@ -0,0 +1,14 @@
+package dhurga;
+
+public class D {
+
+	public static void main(String[] args) {
+		D obj=new D();
+		obj.m1();
+
+	}
+public void m1()
+{
+	System.out.println("my name is dhurga");
+	}
+}
