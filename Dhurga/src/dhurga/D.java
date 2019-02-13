3cf2552f74926b9e8a1bd22abee6974fa576d783
Dhurga <Dhurga@admin-PC>
2019-02-13 14:44:06 +0530

    at dhurga updated

--- Dhurga/src/dhurga/D.java
+++ Dhurga/src/dhurga/D.java
@@ -4,11 +4,16 @@ public class D {
 
 	public static void main(String[] args) {
 		D obj=new D();
-		obj.m1();
+		//obj.m1();
+		obj.m2();
 
 	}
 public void m1()
 {
 	System.out.println("my name is dhurga");
+	}
+public void m2() 
+{
+	System.out.println("I like cricket");
 	}
 }
